1bed492f4705246308662c8a03e6151efa1900fc
sonnym <michaud.sonny@gmail.com>
2013-05-19 21:50:20 -0400

    remove with from language definition
    now implemented as a special case of function definition and application
    
    (Exercise 6.3.1)

--- ch6/interpreter.rkt
+++ ch6/interpreter.rkt
@@ -1,16 +1,15 @@
 #lang plai
 
-;; define FWAE type
-(define-type FWAE
+;; define FAE type
+(define-type FAE
   [num (n number?)]
-  [add (lhs FWAE?) (rhs FWAE?)]
-  [with (name symbol?) (named-expr FWAE?) (body FWAE?)]
+  [add (lhs FAE?) (rhs FAE?)]
   [id (name symbol?)]
-  [fun (param symbol?) (body FWAE?)]
-  [app (fun-expr FWAE?) (arg-expr FWAE?)])
+  [fun (param symbol?) (body FAE?)]
+  [app (fun-expr FAE?) (arg-expr FAE?)])
 
-;; parse : sexp -> FWAE
-;; to convert s-expressions into FWAEs
+;; parse : sexp -> FAE
+;; to convert s-expressions into FAEs
 (define (parse sexp)
   (cond
     [(number? sexp) (num sexp)]
@@ -23,22 +22,18 @@
                   (parse (- (third sexp))))]
         [(fun) (fun (first (second sexp))
                     (parse (third sexp)))]
-        [(with) (with (first (second sexp))
-                      (parse (second (second sexp)))
-                      (parse (third sexp)))]
+        [(with) (app (fun (first (second sexp))
+                          (parse (third sexp)))
+                     (parse (second (second sexp))))]
         [else [app (parse (first sexp)) (parse (second sexp))]])]))
 
-;; interp : FWAE -> FWAE
-;; evaluates FWAE expressions by reducing them to their corresponding values
+;; interp : FAE -> FAE
+;; evaluates FAE expressions by reducing them to their corresponding values
 ;; return values are either number or fun
 (define (interp expr)
-  (type-case FWAE expr
+  (type-case FAE expr
     [num (n) expr]
     [add (l r) (add-numbers (interp l) (interp r))]
-    [with (bound-id named-expr bound-body)
-          (interp (subst bound-body
-                         bound-id
-                         (interp named-expr)))]
     [id (v) (error 'interp "free identifier")]
     [fun (bound-id bound-body)
          expr]
@@ -48,26 +43,18 @@
                           (fun-param fun-val)
                           (interp arg-expr))))]))
 
-;; add-numbers : FWAE FWAE -> FWAE
+;; add-numbers : FAE FAE -> FAE
 (define (add-numbers x y) (num (+ (num-n x) (num-n y))))
 
-;; subst : FWAE symbol FWAE -> FWAE
+;; subst : FAE symbol FAE -> FAE
 ;; substitutes second argument with third argument in first argument
 ;; as per the rules of substitution; the resulting expression contains
 ;; no free instances of the second argument
 (define (subst expr sub-id val)
-  (type-case FWAE expr
+  (type-case FAE expr
     [num (n) expr]
     [add (l r) (add (subst l sub-id val)
                     (subst r sub-id val))]
-    [with (bound-id named-expr bound-body)
-      (if (symbol=? bound-id sub-id)
-          (with bound-id
-                (subst named-expr sub-id val)
-                bound-body)
-          (with bound-id
-                (subst named-expr sub-id val)
-                (subst bound-body sub-id val)))]
     [id (v) (if (symbol=? v sub-id) val expr)]
     [fun (bound-id bound-body)
          (fun bound-id (subst bound-body sub-id val))]
